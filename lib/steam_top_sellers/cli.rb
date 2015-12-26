b7552a6724a1ee59c7eb210015077af8291e10ef
Mitul Mistry <mitul.mistry1@gmail.com>
2015-12-26 00:11:15 +0000

    blocked in CLI methods

--- lib/steam_top_sellers/cli.rb
+++ lib/steam_top_sellers/cli.rb
@@ -1,6 +1,25 @@
-require_relative "../lib/game.rb"
-require_relative "../lib/scraper.rb"
+require_relative "game.rb"
+require_relative "scraper.rb"
 
 class CommandLineInteface
 
+  def run
+    Game.create_from_steam_api
+    list_games
+  end
+
+  def list_games
+    puts "--------- STEAM TOP SELLERS ---------"
+    Game.all.each.with_index(1) do |game, index| #each.with_index(1) allows you to offset the index, unlike each_with_index
+      puts "#{index}. #{game.title} - $#{game.price}"
+    end
+
+  end
+=begin
+  def print_game(game)
+
+  end
+=end
 end
+
+CommandLineInteface.new.run
